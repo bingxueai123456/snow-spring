29d041cb00cdc7944bde17b1bf02c60ab08cc49b
eclair <eclair@qq.com>
2024-06-23 11:02:06 +0800

    Initial commit

---
+++ src/main/java/com.ice/App.java
@@ -0,0 +1,15 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : eclair
+ * @date : 2024/6/21 17:25
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
